ea425f7f45ed6aefe4668aeb77fbe0f28e5d85af
dickson7 <garciadickson@hotmail.com>
2019-04-10 17:09:26 -0500

    form animado con javascript

---
+++ javascript/formanimation/js/app.js
@@ -0,0 +1,63 @@
+function animatedForm(){
+    const arrows = document.querySelectorAll(".fa-arrow-circle-down");
+
+    arrows.forEach(arrow => {
+        arrow.addEventListener("click", () => {
+            const input = arrow.previousElementSibling;
+            const parent = arrow.parentElement;
+            //console.log(input);
+            //console.log(parent);
+            const nextForm = parent.nextElementSibling;
+
+            // se valida al realizar el evento
+            if(input.type === "text" && validateUser(input)){
+                console.log("todo salio ok");
+                nextSlide(parent, nextForm);
+            }else if(input.type === "email" && validateEmail(input)){
+                nextSlide(parent, nextForm);
+            } else if(input.type === "password" && validateUser(input)){
+                nextSlide(parent, nextForm);
+            }else{
+                //se llama la animacion
+                parent.style.animation="shake 0.5s ease";
+            }
+            // se deshace la animacion
+            parent.addEventListener("animationend", () => {
+                parent.style.animation = "";
+            });
+        });
+    });
+}
+
+function validateUser (user){
+    if (user.value.length < 6){
+        console.log('No hay suficientes caracters')
+        error('rgb(189, 87,87)');
+    }else{
+        error("rgb(87, 189, 130)");
+        return true;
+    }
+}
+
+function validateEmail(email){
+    const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (validation.test(email.value)){
+        error("rgb(87, 189, 130)");
+        return true;
+    }else{
+        error('rgb(189, 87,87)');
+    }
+}
+
+function nextSlide(parent, nextForm){
+    parent.classList.add("innactive");
+    parent.classList.add("active");
+    nextForm.classList.add("active");
+}
+
+function error(color){
+    document.body.style.backgroundColor= color;
+}
+
+
+animatedForm();
