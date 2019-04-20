50d864f5dcc8875a5a1f5f86b2ea9a6057f05c38
dickson7 <garciadickson@hotmail.com>
2019-04-19 19:11:20 -0500

    update scroll animation

--- javascript/scrollanimation/app.js
+++ javascript/scrollanimation/app.js
@@ -13,7 +13,23 @@ const flightPath = {
     ]
 };
 
+const flightPath2 = {
+    curviness: 2.5,
+    autoRotate:false,
+    values: [
+        {x: -100, y: 20}, 
+        {x: -300, y: 10},
+        {x: -400, y: 0},
+        {x: -500, y:10},
+        {x: -600, y: 20},
+        {x: -800, y: 10},
+        {x: -1800, y: 0},
+        {x: window.innerWidth, y:10}
+    ]
+};
+
 const tween = new TimelineLite();
+const tween2 = new TimelineLite();
 
 tween.add(
     TweenLite.to(".paper-plane", 1, {
@@ -22,14 +38,23 @@ tween.add(
     })
 );
 
+tween2.add(
+    TweenLite.to(".nube-plane", 2, {
+        bezier: flightPath2,
+        ease: Power1.easeInOut
+    })
+);
+
+
 const controller = new ScrollMagic.Controller();
+const controller2 = new ScrollMagic.Controller();
 
 const scene = new ScrollMagic.Scene({
     triggerElement: ".animation",
-    duration: 500,
+    duration: 1000,
     triggerHook: 0
 })
 .setTween(tween)
-.addIndicators()
+//.addIndicators()
 .setPin(".animation")
 .addTo(controller);
