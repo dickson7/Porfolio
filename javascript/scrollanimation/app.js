9639ef3d9285b553d5b2c343d69129a69694b318
dickson7 <garciadickson@hotmail.com>
2019-04-20 20:50:10 -0500

    update testimoniales y scrollanimation

--- javascript/scrollanimation/app.js
+++ javascript/scrollanimation/app.js
@@ -1,4 +1,4 @@
-const flightPath = {
+flightPath = {
     curviness: 2.5,
     autoRotate:true,
     values: [
@@ -13,23 +13,8 @@ const flightPath = {
     ]
 };
 
-const flightPath2 = {
-    curviness: 2.5,
-    autoRotate:false,
-    values: [
-        {x: -100, y: 20}, 
-        {x: -300, y: 10},
-        {x: -400, y: 0},
-        {x: -500, y:10},
-        {x: -600, y: 20},
-        {x: -800, y: 10},
-        {x: -1800, y: 0},
-        {x: window.innerWidth, y:10}
-    ]
-};
 
 const tween = new TimelineLite();
-const tween2 = new TimelineLite();
 
 tween.add(
     TweenLite.to(".paper-plane", 1, {
@@ -38,16 +23,9 @@ tween.add(
     })
 );
 
-tween2.add(
-    TweenLite.to(".nube-plane", 2, {
-        bezier: flightPath2,
-        ease: Power1.easeInOut
-    })
-);
-
 
 const controller = new ScrollMagic.Controller();
-const controller2 = new ScrollMagic.Controller();
+
 
 const scene = new ScrollMagic.Scene({
     triggerElement: ".animation",
