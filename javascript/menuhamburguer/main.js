74200a18e984a9c6cb2758d5668f47dc07d8935f
dickson7 <garciadickson@hotmail.com>
2019-05-05 22:10:37 -0500

    landing page 2 menu burger

--- javascript/menuhamburguer/main.js
+++ javascript/menuhamburguer/main.js
@@ -1,26 +1,3 @@
-// function parallax(element, distance, speed){
-//     const item = document.querySelector(element);
-//     item.style.transform = `translateY(${distance * speed}px)`;
-// }
-
-// window.addEventListener("scroll", function(){
-//     parallax(".parallax", ".trivial", 1);
-//     parallax(".img-trivial", window.scrollY, 0.4);
-// });
-
-
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 200) {
-        $(".header-area").addClass("scroll-header");
-    }
-    if (scroll < 150) {
-        $(".header-area").removeClass("scroll-header");
-    }
-});
-
-
 const navSlide =() => {
     const burguer = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
